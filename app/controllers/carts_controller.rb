8afc383f3842780de3e2b963747c1fd374dab9f3
JuanDavidDV <juandadazav@gmail.com>
2024-10-23 14:17:19 -0400

    checkout payment feature within cart added

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,44 @@
 class CartsController < ApplicationController
   def create
+    @product = Product.find(params[:product_id])
+    @current_cart.cart_items.create(product_id: @product.id)
+  end
+
+  def show
+  end
+
+  def checkout
+  end
+
+  def stripe_session
+    session = Stripe::Checkout::Session.create({
+    ui_mode: 'embedded',
+    line_items: [{
+      # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+        price_data: {
+          currency: "cad",
+          unit_amount: (@current_cart.products.sum(&:price) * 100).to_i,    #Stripe does not use decimals, thefore this will be multipled by 100 and converted to intiger
+          product_data: {
+            name: @current_cart.products.map(&:name).join(", ")
+          },
+        },
+        quantity: 1,
+      }],
+      shipping_address_collection: {
+        allowed_countries: STRIPE_SUPPORTED_COUNTRIES
+      },
+      mode: 'payment',
+      return_url: success_cart_url(@current_cart.secret_id),    #This where the user will be redirected after paying
+    })
+
+    render json: { clientSecret: session.client_secret }
+  end
+
+  def success
+    if @current_cart.cart_items.any?
+      session[:current_cart_id] = nil
+    end
+    @purchased_cart = Cart.find_by_secret_id(params[:id])
+    redirect_to root_path if !@purchased_cart
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,14 @@
 class Cart < ApplicationRecord
+  has_many :cart_items, dependent: :destroy #Deletes children
+  has_many :products, through: :cart_items
+
+  before_create :set_secret_id
+
+  enum :status, ["pending", "complete"]
+
+  private 
+
+  def set_secret_id
+    self.secret_id = SecureRandom.uuid() + DateTime.now.to_i.to_s
+  end
 end

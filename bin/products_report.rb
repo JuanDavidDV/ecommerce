caaa146487eb18e0089c20d0603ea044599ac043
JuanDavidDV <juandadazav@gmail.com>
2024-11-06 23:14:43 -0500

    csv feature added

--- bin/products_report.rb
+++ bin/products_report.rb
@@ -7,7 +7,7 @@ class ProductReport
   end
 
   def call
-    products_report_csv   # Generates csv report
+    # products_report_csv   # Generates csv report
 
     delete_products_records   # Deletes all records from Product table
 
@@ -17,14 +17,15 @@ class ProductReport
 
   private
 
-  def products_report_csv
-    CSV.open("products_report_#{Date.today}.csv", "wb", headers: true) do |product_report|  # Setup CSV file
-      product_report << ["Name", "Price", "Category"] # Adds headers to csv file
+  def productsreport_csv
+    CSV.open("products_report#{Date.today}.csv", "wb", headers: true) do |productreport|  # Setup CSV file
+      product_report << ["ID", "Name", "Price", "Category", "Category ID"] # Adds headers to csv file
 
       Product.all.each do |p|
-        product_report << [p.name, p.price, p.category.name]  # Adds records from fields Product and Category table
+        product_report << [p.id, p.name, p.price, p.category.name, p.category_id]  # Adds records from fields Product and Category table
       end
     end 
+    
     puts "Products report has been successfully created"
   end
 
@@ -32,23 +33,22 @@ class ProductReport
     Product.find_each do |product|
       product.destroy!
     end
+
     puts "Product records have been deleted"
   end
 
   def import_products_csv
-    report_csv = "products_report_#{Date.today}.csv"
+    report_csv = "products_report#{Date.today}.csv"
 
     CSV.foreach(report_csv, headers: true) do |row| # Open and access the records of csv file
-      product = Product.find_or_create_by(name: row["Name"])  # Considering that the product name is the primary key of the table, this will prevent duplicates and update exisiting records if found with the same product name
-      
-      product.price = row["Price"]
-      
-      category_name = row["Category"]
-      category = Category.find_or_create_by(name: category_name)
-      product.category = category   # Stablish foreign key
+      product = Product.find_or_initialize_by(id: row["ID"])  # Considering that the product name is the primary key of the table, this will prevent duplicates and update exisiting records if found with the same product name
+      category = Category.find_or_create_by!(id: row["Category ID"])
 
-      product.save # Save the product
+      product.price = row["Price"]
+      product.category = category   # Stablish foreign key
+      product.save! # Save the product
     end
+
     puts "Prodcuts have been imported from csv report"
   end
 

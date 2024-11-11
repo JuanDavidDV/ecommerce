96c322d7ab9cb1c6b3e34baf81471d8b8852362a
JuanDavidDV <juandadazav@gmail.com>
2024-11-11 03:02:32 -0500

    remove policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -23,8 +23,3 @@
 #   # Report violations without enforcing the policy.
 #   # config.content_security_policy_report_only = true
 # end
-Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self
-  policy.script_src :self, "https://js.stripe.com"
-  policy.connect_src :self, "https://api.stripe.com"
-end

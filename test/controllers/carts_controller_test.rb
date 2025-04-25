9532f391681d636be388d2c5839b7af4dd82d6e3
JuanDavidDV <juandadazav@gmail.com>
2025-04-25 19:16:32 -0400

    create setup

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
 class CartsControllerTest < ActionDispatch::IntegrationTest
-  
+  setup do
+    @product = products(:one)
+    @user = users(:one)
+    @cart = Cart.create
+    @cart.cart_items.create(product: @product)
+    @empty_cart = Cart.create
+  end
+
+
 end

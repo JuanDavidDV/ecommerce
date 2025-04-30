debcd9a1dcd7dc2f9d84f847d9b9babb10802574
JuanDavidDV <juandadazav@gmail.com>
2025-04-30 09:40:26 -0400

    create test to redirect when cart is empty

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -31,5 +31,10 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "should remove item "
+  test "should redirect to checkout if cart is empty" do
+    post cart_path(product_id: @product.id) # Simulates post request to create a cart and set the session
+    CartItem.delete_all
+    get checkout_cart_path
+    assert_redirected_to root_path
+  end
 end

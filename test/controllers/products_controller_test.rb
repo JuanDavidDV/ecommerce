1223f5239b893ceee4e2e5c6d2f0e27f8cd7fd49
JuanDavidDV <juandadazav@gmail.com>
2025-05-01 10:10:19 -0400

    redesign products controller and create test to get index (root) of all products

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,46 +3,11 @@ require "test_helper"
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @admin = admins(:one)
   end
 
-  test "should get index" do
-    get products_url
+  test "should get index of the products" do
+    get root_url
     assert_response :success
-  end
-
-  test "should get new" do
-    get new_product_url
-    assert_response :success
-  end
-
-  test "should create product" do
-    assert_difference("Product.count") do
-      post products_url, params: { product: { name: @product.name, price: @product.price } }
-    end
-
-    assert_redirected_to product_url(Product.last)
-  end
-
-  test "should show product" do
-    get product_url(@product)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_product_url(@product)
-    assert_response :success
-  end
-
-  test "should update product" do
-    patch product_url(@product), params: { product: { name: @product.name, price: @product.price } }
-    assert_redirected_to product_url(@product)
-  end
-
-  test "should destroy product" do
-    assert_difference("Product.count", -1) do
-      delete product_url(@product)
-    end
-
-    assert_redirected_to products_url
   end
 end

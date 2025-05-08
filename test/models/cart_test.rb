3d41d8fbbc6e1a6985039dd0c6b1923dc729aa19
JuanDavidDV <juandadazav@gmail.com>
2025-05-08 09:52:04 -0400

    create setup for cart test controller

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @cart = carts(:one)
+  end
+  
 end

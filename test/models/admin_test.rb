8625abf6dc319b789a76c21d8bd00a22b1546b4a
JuanDavidDV <juandadazav@gmail.com>
2025-05-08 08:43:06 -0400

    finish test for admin model

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @admin = admins(:one)
+  end
+
+  test "should be valid with appropiate attributes" do
+    assert @admin.valid?
+  end
 end

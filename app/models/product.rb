7b35aff47be816298894efb899a7804c2e0be220
JuanDavidDV <juandadazav@gmail.com>
2025-05-10 16:13:05 -0400

    add validation for name and price for the Product mmodel

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,7 @@ class Product < ApplicationRecord
   has_many_attached :images
   belongs_to :category, optional: true  # Without "optional: true" it will be mandatory to belong to category
   has_many :cart_items, dependent: :destroy # Deletes all cart_items associated when a product is deleted. this to resolve error: PG::ForeignKeyViolation
+
+  validates :name, presence: true
+  validates :price, presence: true
 end

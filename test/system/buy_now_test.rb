2c40c575d31ca70b206495d36612eaed3791ad5a
JuanDavidDV <juandadazav@gmail.com>
2025-05-16 20:40:28 -0400

    create test to get show page

--- test/system/buy_now_test.rb
+++ test/system/buy_now_test.rb
@@ -5,4 +5,11 @@ class BuyNowTest < ApplicationSystemTestCase
     @product = products(:one)
     @user = users(:one)
   end
+
+  test "should get show page" do
+    visit product_buy_now_url(@product)
+    using_wait_time(10) do
+      assert_text "Soccer"
+    end
+  end
 end

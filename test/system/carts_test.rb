0d31b7dd55e6ab8e7c93df7001481ec7e4de5bb8
JuanDavidDV <juandadazav@gmail.com>
2025-05-17 22:27:38 -0400

    create test to add 1 item to a cart

--- test/system/carts_test.rb
+++ test/system/carts_test.rb
@@ -6,4 +6,13 @@ class CartsTest < ApplicationSystemTestCase
     @cart = carts(:one)
     @user = users(:one)
   end
+
+  test "should add one product to a cart" do
+    visit product_url(@product)
+    using_wait_time(10) do
+      click_on "Add To Cart"
+    end
+
+    assert_selector "div.bg-white.border.border-black", text: "1", wait: 5
+  end
 end
